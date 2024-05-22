8ae5556c0379919e8561fe890b01ac11699cf4ba
chair00 <pk1029@yu.ac.kr>
2024-05-22 19:16:35 +0900

    test commit

--- src/main/java/com/example/demo/controller/ExController.java
+++ src/main/java/com/example/demo/controller/ExController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World Test Success??";
+        return "Hello World Test Success?";
     }
 }
