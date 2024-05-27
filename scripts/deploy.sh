fdc4851eca6cef5f9c4372cee9eb4a0e5fe30775
chair00 <101685159+chair00@users.noreply.github.com>
2024-05-27 13:55:20 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,6 +16,20 @@ else
   sleep 5
 fi
 
+echo "> 현재 구동 중인 8080 pid 확인"
+
+CURRENT_PID=$(netstat -tnlp | grep 8080 | awk '{print $1}')
+
+echo "현재 구동 중인 8080 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
 echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
