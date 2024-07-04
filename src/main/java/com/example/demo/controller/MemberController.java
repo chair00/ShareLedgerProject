c68977d7fb2a6a776da91dd50fe94ad148717c98
chair00 <pk1029@yu.ac.kr>
2024-07-04 13:38:24 +0900

    update

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.LedgerDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,26 +10,26 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     // 회원가입
     @PostMapping("/signin")
-    public String signin(){
-        return "회원가입";
+    public ResponseEntity<?> signin(){
+        return ResponseEntity.ok().body("회원가입");
     }
 
     // 로그인
     @PostMapping("/login")
-    public String login(){
-        return "로그인";
+    public ResponseEntity<?> login(){
+        return ResponseEntity.ok().body("로그인");
     }
 
     // 로그아웃
     @PostMapping("/logout")
-    public String logout(){
-        return "로그인";
+    public ResponseEntity<?> logout(){
+        return ResponseEntity.ok().body("로그아웃");
     }
 
     // 탈퇴
     @PostMapping("/withdraw")
-    public String withdraw(){
-        return "탈퇴";
+    public ResponseEntity<?> withdraw(){
+        return ResponseEntity.ok().body("탈퇴");
     }
 
 }
