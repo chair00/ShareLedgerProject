f1710027f211d87c20737f3c4f38632873609267
chair00 <pk1029@yu.ac.kr>
2024-07-18 01:07:13 +0900

    write test code.. need to update

---
+++ src/test/java/com/example/demo/controller/CategoryControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.CategoryDto;
+import com.example.demo.entity.Category;
+import com.example.demo.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryControllerTest {
+    /*
+    @Autowired
+    private final CategoryRepository categoryRepository;
+
+
+    @Test
+    void createCategory() {
+
+
+        // given
+        CategoryDto categoryDto = CategoryDto.builder().name("hihi").type("hih").build();
+
+        // when
+        Category category = categoryRepository.save(categoryDto.toEntity());
+
+        // then
+        assertEquals("", category.getName());
+    }
+
+    @Test
+    void testCreateCategory() {
+    }
+    */
+
+}
