0f81a169343cc4dae40af2b2ad24b8090d685883
chair00 <pk1029@yu.ac.kr>
2024-07-18 16:00:37 +0900

    swagger 설명 추가

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -5,6 +5,8 @@ import com.example.demo.dto.CategoryReqDto;
 import com.example.demo.dto.CategoryResDto;
 import com.example.demo.dto.SubCategoryReqDto;
 import com.example.demo.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,13 @@ import java.util.List;
 @RequestMapping("/ledger")
 @RequiredArgsConstructor
 
+@Tag(name = "카테고리", description = "카테고리 API")
 public class CategoryController {
 
     private final CategoryService categoryService;
 
     // 카테고리 추가
+    @Operation(summary = "카테고리 추가", description = "가계부 내 상위 카테고리를 생성한다.")
     @PostMapping("/{ledgerId}/category")
     public ResponseEntity<Long> createCategory(@PathVariable Long ledgerId, @Valid @RequestBody CategoryReqDto categoryReqDto){
         Long createCategoryId = categoryService.save(ledgerId, categoryReqDto);
@@ -29,6 +33,7 @@ public class CategoryController {
     }
 
     // 서브 카테고리 추가
+    @Operation(summary = "서브 카테고리 추가", description = "가계부에 생성된 상위 카테고리 내 하위 카테고리를 생성한다. {categoryId}는 상위 카테고리 id를 의미한다.")
     @PostMapping("/{ledgerId}/category/{categoryId}")
     public ResponseEntity<Long> createCategory(@PathVariable Long ledgerId, @PathVariable Long categoryId, @Valid @RequestBody SubCategoryReqDto categoryReqDto){
         Long createCategoryId = categoryService.save(ledgerId, categoryId, categoryReqDto);
@@ -36,6 +41,7 @@ public class CategoryController {
     }
 
     // 카테고리 상세 조회
+    @Operation(summary = "카테고리 상세 조회", description = "가계부 내 카테고리 정보를 상세 조회한다. 상위 카테고리의 경우 하위 카테고리 리스트도 함께 조회한다.")
     @GetMapping("/{ledgerId}/category/{categoryId}")
     public ResponseEntity<CategoryResDto> getCategory(@PathVariable Long categoryId){
         CategoryResDto category = categoryService.find(categoryId);
@@ -43,6 +49,7 @@ public class CategoryController {
     }
 
     // 가계부 별 카테고리 목록 조회(서브 카테고리 제외)
+    @Operation(summary = "카테고리 목록 조회", description = "가계부 내 카테고리 목록를 조회한다. 하위 카테고리는 제외한다.")
     @GetMapping("/{ledgerId}/category")
     public ResponseEntity<List<CategoryListResDto>> getCategoryList(@PathVariable Long ledgerId){
         List<CategoryListResDto> categoryList = categoryService.findAll(ledgerId);
@@ -50,6 +57,7 @@ public class CategoryController {
     }
 
     // 카테고리 수정
+    @Operation(summary = "카테고리 수정", description = "가계부 내 카테고리 정보를 수정한다.")
     @PutMapping("/{ledgerId}/category/{categoryId}")
     public ResponseEntity<Long> updateCategory(@PathVariable Long categoryId, @RequestBody CategoryReqDto categoryReqDto){
         Long updatedCategoryId = categoryService.update(categoryId, categoryReqDto);
@@ -57,6 +65,7 @@ public class CategoryController {
     }
 
     // 카테고리 삭제
+    @Operation(summary = "카테고리 삭제", description = "가계부 내 카테고리를 삭제한다.")
     @DeleteMapping("/{ledgerId}/category/{categoryId}")
     public ResponseEntity<?> deleteCategory(@PathVariable Long categoryId){
         categoryService.delete(categoryId);
