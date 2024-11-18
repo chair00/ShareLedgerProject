3f2f9b8f3c8714abee3151ac9b8cdb9b34241f95
chair00 <pk1029@yu.ac.kr>
2024-11-18 21:13:56 +0900

    permitAll

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .requestMatchers("/login", "/", "/signup", "/h2-console/**", "/token", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/", "/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 );
